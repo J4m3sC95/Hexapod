80db245c2b362fb44247fb21d09382361a3c748b
James <jcoulson95@gmail.com>
2016-12-21 20:54:21 +0000

    more data collection for current sensor testing

--- current_sensor_test/current_sensor_test.ino
+++ current_sensor_test/current_sensor_test.ino
@@ -1,17 +1,18 @@
-float data;
+float data1, data2;
 
 void setup() {
-  Serial.begin(250000);
+  Serial.begin(115200);
 }
 
 void loop() {
-  data = analogRead(A0);
-  //Serial.print("Raw: \t");
-  //Serial.print(analogRead(A0));
-  //Serial.print(",\tcurrent: ");
-  //Serial.print(", ");
-  Serial.print(((512-data)*5)/(0.185*1024));
+  data1 = analogRead(A0);
+  data2 = analogRead(A1);
+  Serial.print(millis());
   Serial.print(",");
-  Serial.println(millis());
+  Serial.print(((512-data1)*5)/(0.185*1024));
+  Serial.print(",");
+  Serial.print(((512-data2)*5)/(0.185*1024));
+  Serial.println("");
+  
   //Serial.println("");
 }
